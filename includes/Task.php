fcfaae167eabb2332a8d8fa9a5e88e17b8f41b4a
Mauro Van de kerckhoven <r1051327@student.thomasmore.be>
2026-05-19 16:13:00 +0200

    task

---
+++ includes/Task.php
@@ -0,0 +1,80 @@
+<?php
+
+require_once("Db.php");
+
+class Task {
+
+    private $title;
+    private $priority;
+    private $listId;
+
+    public function setTitle($title) {
+
+        if(empty($title)) {
+            throw new Exception("Task title cannot be empty");
+        }
+
+        $this->title = $title;
+    }
+
+    public function getTitle() {
+        return $this->title;
+    }
+
+    public function setPriority($priority) {
+        $this->priority = $priority;
+    }
+
+    public function getPriority() {
+        return $this->priority;
+    }
+
+    public function setListId($listId) {
+        $this->listId = $listId;
+    }
+
+    public function getListId() {
+        return $this->listId;
+    }
+
+    public function create() {
+
+        $conn = Db::getConnection();
+
+        $statement = $conn->prepare("
+            INSERT INTO tasks
+            (list_id, title, priority)
+            VALUES
+            (:list_id, :title, :priority)
+        ");
+
+        $statement->bindValue(":list_id", $this->listId);
+        $statement->bindValue(":title", $this->title);
+        $statement->bindValue(":priority", $this->priority);
+
+        return $statement->execute();
+    }
+
+    public static function getTasksByList($listId) {
+
+        $conn = Db::getConnection();
+
+        $statement = $conn->prepare("
+            SELECT * FROM tasks
+            WHERE list_id = :list_id
+
+            ORDER BY
+            CASE
+                WHEN priority = 'high' THEN 1
+                WHEN priority = 'medium' THEN 2
+                ELSE 3
+            END
+        ");
+
+        $statement->bindValue(":list_id", $listId);
+
+        $statement->execute();
+
+        return $statement->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
